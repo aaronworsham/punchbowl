243d6826dd72e12cd00bfd5b0d9730396cfa4d06
Aaron Worsham <aaronworsham@gmail.com>
2010-09-23 18:42:32 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Punchbowl::Application.config.secret_token = '62f53923919423fe98dcb397500b2813522383ed2be1bfb75a3e9aa6124e1bf76bee523ce8f8fb0a3ffb260d9be93dce75af208e00b6799f85723d239119ee25'
