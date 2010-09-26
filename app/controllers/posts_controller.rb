5d435a2d027828c5be6257ef62941dfb00c4239d
Aaron Worsham <aaronworsham@gmail.com>
2010-09-26 12:50:20 -0400

    missed the path addition to the url creator in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,9 +7,9 @@ class PostsController < ApplicationController
     post = Post.create(params[:post].merge(:customer => customer))
 
     if facebook_post? 
-      redirect_to auth_facebook_post(post, :post_to => paramify_post_to)
+      redirect_to auth_facebook_post_path(post, :post_to => paramify_post_to)
     elsif twitter_post?
-      redirect_to auth_twitter_post(post, :post_to => paramify_post_to)
+      redirect_to auth_twitter_post_path(post, :post_to => paramify_post_to)
     end
   end
 end
