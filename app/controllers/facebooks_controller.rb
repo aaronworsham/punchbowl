10f017a8f260125a996555c383fa688d139563b9
Aaron Worsham <aaronworsham@gmail.com>
2010-09-26 15:09:01 -0400

    swap response with response.body

--- app/controllers/facebooks_controller.rb
+++ app/controllers/facebooks_controller.rb
@@ -39,7 +39,7 @@ class FacebooksController < ApplicationController
     Rails.logger.error e.response.body 
     Rails.logger.error e.response.headers
     if e.respond_to?("response") 
-      flash[:warning] = e.response.present? ? JSON.parse(e.response)["error"]["message"] : e.message
+      flash[:warning] = e.response.present? ? JSON.parse(e.response.body)["error"]["message"] : e.message
     else
       flash[:warning] = e.message
     end
