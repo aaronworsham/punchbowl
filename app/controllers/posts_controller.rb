7c012c5a2948cf96f6d2db697f022c278eededad
Aaron Worsham <aaronworsham@gmail.com>
2010-09-27 19:49:04 -0400

    alter routing from post_controller based on having a token and id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,12 @@ class PostsController < ApplicationController
     customer = Customer.find_or_create_by_email(:email => email)
     post = Post.create(params[:post].merge(:customer => customer))
 
-    if facebook_post? 
-      redirect_to auth_post_facebook_path(post, :post_to => paramify_post_to)
+    if facebook_post?
+      if customer.facebook_token.present? and customer.facebook_id.present? 
+        redirect_to post_message_post_facebook_path(post, :post_to => paramify_post_to)
+      else
+        redirect_to auth_post_facebook_path(post, :post_to => paramify_post_to)
+      end
     elsif twitter_post?
       redirect_to auth_post_twitter_path(post, :post_to => paramify_post_to)
     end
