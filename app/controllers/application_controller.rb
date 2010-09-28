d029c84366bf7ec7e240064f5b861635dff5bafc
Aaron Worsham <aaronworsham@gmail.com>
2010-09-28 17:32:43 -0400

    added green light check to post_to_social_media

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,13 +15,13 @@ class ApplicationController < ActionController::Base
 
   def post_to_social_media
     if @post.posted_to_facebook?
-      if @customer.facebook_account 
+      if @customer.facebook_account and @customer.facebook_account.green_light? 
         redirect_to post_message_post_facebook_path(@post)
       else
         redirect_to auth_post_facebook_path(@post)
       end
     elsif @post.posted_to_twitter?
-      if @customer.twitter_account
+      if @customer.twitter_account and @customer.twitter_account.green_light?
         redirect_to post_message_post_twitter_path(@post)
       else
         redirect_to auth_post_twitter_path(@post)
