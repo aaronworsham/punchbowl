8a9a87c45006586a0a9b06ecc2e2bccde8b956ac
Aaron Worsham <aaronworsham@gmail.com>
2010-09-27 22:17:06 -0400

    fix app config

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -1,6 +1,6 @@
 class SystemMailer < ActionMailer::Base
-  default :from => AppConfig.email.default.from 
-  default :to   => AppConfig.email.default.to 
+  default :from => AppConfig.email["default"]["from"]
+  default :to   => AppConfig.email["default"]["to"]
   def warning_email(msg)
     @msg = msg
     mail(:subject => "A warning from Punchbowl")
