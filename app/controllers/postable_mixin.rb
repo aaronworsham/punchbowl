be5e9548219d92f295c8ddd9fa39a681f400e515
Aaron Worsham <aaronworsham@gmail.com>
2010-09-29 08:25:35 -0400

    route error and more logging

--- app/controllers/postable_mixin.rb
+++ app/controllers/postable_mixin.rb
@@ -2,17 +2,21 @@ module PostableMixin
 
   def post_to_social_media
     if @post.posted_to_facebook?
-      if @customer.facebook_account and @customer.facebook_account.green_light? 
+      if @customer.facebook_account and @customer.facebook_account.green_light?
+        Rails.logger.info "Postable = Facebook greenlit" 
         conn.get post_message_post_facebook_path(@post)
         redirect_to @post.success_url
       else
+        Rails.logger.info "Postable = Facebook redlit" 
         redirect_to auth_post_facebook_path(@post)
       end
     elsif @post.posted_to_twitter?
       if @customer.twitter_account and @customer.twitter_account.green_light?
+        Rails.logger.info "Postable = twitter greenlit" 
         conn.get post_message_post_twitter_path(@post)
         redirect_to @post.success_url
       else
+        Rails.logger.info "Postable = Facebook redlit" 
         redirect_to auth_post_twitter_path(@post)
       end
     end
