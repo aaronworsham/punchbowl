65807916922956f11aeadaff57445dfcabfb4bd9
Aaron Worsham <aaronworsham@gmail.com>
2012-05-16 12:17:26 -0400

    better error handling

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -51,65 +51,77 @@ class CustomersController < ApplicationController
 
   def add_network
     @customer = Customer.find_by_uuid params[:uuid]
-    case params[:network]
-      when 'facebook'
-        unless @customer.facebook_user?
-          @customer.update_attribute(:facebook_user, true)
-          start_authorizing('facebook')
-          render :json => {:network => 'facebook', :added => true, :url => auth_url('facebook')}
-        else
-          render :json => {:network => 'facebook', :added => false, :error => 'Already facebook user'}
-        end
-      when 'twitter'
-        unless @customer.twitter_user?
-          @customer.update_attribute(:twitter_user, true)
-          start_authorizing('twitter')
-          render :json => {:network => 'twitter', :added => true, :url => auth_url('twitter')}
-        else
-          render :json => {:network => 'twitter', :added => false, :error => 'Already twitter user'}
-        end
+    if @customer
+      case params[:network]
+        when 'facebook'
+          unless @customer.facebook_user?
+            @customer.update_attribute(:facebook_user, true)
+            start_authorizing('facebook')
+            render :json => {:network => 'facebook', :added => true, :url => auth_url('facebook')}
+          else
+            render :json => {:network => 'facebook', :added => false, :error => 'Already facebook user'}
+          end
+        when 'twitter'
+          unless @customer.twitter_user?
+            @customer.update_attribute(:twitter_user, true)
+            start_authorizing('twitter')
+            render :json => {:network => 'twitter', :added => true, :url => auth_url('twitter')}
+          else
+            render :json => {:network => 'twitter', :added => false, :error => 'Already twitter user'}
+          end
+      end
+    else
+      render :json => {:network => params[:network], :removed => false, :error => "Could not locate Customer with uuid #{params[:uuid]}"}
     end
   end
 
   def remove_network
     @customer = Customer.find_by_uuid params[:uuid]
-    case params[:network]
-      when 'facebook'
-        if @customer.facebook_user?
-          @customer.update_attribute(:facebook_user, false)
-          deauthroize('facebook')
-          render :json => {:network => 'facebook', :removed => true}
-        else
-          render :json => {:network => 'facebook', :removed => false, :error => 'Not a Facebook user'}
-        end
-      when 'twitter'
-        if @customer.twitter_user?
-          @customer.update_attribute(:twitter_user, false)
-          deauthroize('twitter')
-          render :json => {:network => 'twitter', :removed => true}
-        else
-          render :json => {:network => 'twitter', :removed => false, :error => 'Not a Twitter user'}
-        end
+    if @customer
+      case params[:network]
+        when 'facebook'
+          if @customer.facebook_user?
+            @customer.update_attribute(:facebook_user, false)
+            deauthroize('facebook')
+            render :json => {:network => 'facebook', :removed => true}
+          else
+            render :json => {:network => 'facebook', :removed => false, :error => 'Not a Facebook user'}
+          end
+        when 'twitter'
+          if @customer.twitter_user?
+            @customer.update_attribute(:twitter_user, false)
+            deauthroize('twitter')
+            render :json => {:network => 'twitter', :removed => true}
+          else
+            render :json => {:network => 'twitter', :removed => false, :error => 'Not a Twitter user'}
+          end
+      end
+    else
+      render :json => {:network => params[:network], :removed => false, :error => "Could not locate Customer with uuid #{params[:uuid]}"}
     end
   end
 
   def reauthorize_network
     @customer = Customer.find_by_uuid params[:uuid]
-    case params[:network]
-      when 'facebook'
-        if @customer.facebook_user?
-          start_authorizing('facebook')
-          render :json => {:network => 'facebook',:authorizing => true, :url => auth_url('facebook')}
-        else
-          render :json => {:network => 'facebook', :authorizing => false, :error => 'Not a Facebook user'}
-        end
-      when 'twitter'
-        if @customer.twitter_user?
-          start_authorizing('twitter')
-          render :json => {:network => 'twitter',:authorizing => true, :url => auth_url('twitter')}
-        else
-          render :json => {:network => 'twitter', :authorizing => false, :error => 'Not a Twitter user'}
-        end
+    if @customer
+      case params[:network]
+        when 'facebook'
+          if @customer.facebook_user?
+            start_authorizing('facebook')
+            render :json => {:network => 'facebook',:authorizing => true, :url => auth_url('facebook')}
+          else
+            render :json => {:network => 'facebook', :authorizing => false, :error => 'Not a Facebook user'}
+          end
+        when 'twitter'
+          if @customer.twitter_user?
+            start_authorizing('twitter')
+            render :json => {:network => 'twitter',:authorizing => true, :url => auth_url('twitter')}
+          else
+            render :json => {:network => 'twitter', :authorizing => false, :error => 'Not a Twitter user'}
+          end
+      end
+    else
+      render :json => {:network => params[:network], :removed => false, :error => "Could not locate Customer with uuid #{params[:uuid]}"}
     end
   end
 
